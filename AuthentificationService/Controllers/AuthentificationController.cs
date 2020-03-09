f4fa99d83534e9d56141a156847cce0dcb9cda50
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2020-03-09 11:20:59 +0300

    Started AuthService feature

---
+++ AuthentificationService/Controllers/AuthentificationController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthentificationService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthentificationController : ControllerBase
+    {
+        private List<string> tokens;
+
+        public AuthentificationController()
+        {
+            tokens = new List<string>();
+
+            for (var i = 0; i < 100; ++i)
+            {
+                tokens.Add("token" + i.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Returns list of existing tokens (just an example)
+        /// </summary>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(0, tokens.Count - 1).Select(index => tokens[index])
+            .ToArray();
+        }
+    }
+}
--- AuthentificationService/Program.cs
+++ AuthentificationService/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace BrokerServices
+namespace AuthentificationService
 {
     public class Program
     {
