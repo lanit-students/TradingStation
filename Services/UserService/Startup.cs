459c3973c093e4c7be63151d2b5a106cffa9ce61
ifelseelif <yougrysha@gmail.com>
2020-03-16 00:50:38 +0300

    add TokenMiddleware

--- Services/UserService/Startup.cs
+++ Services/UserService/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using UserService.Utils;
 
 namespace UserService
 {
@@ -21,6 +22,8 @@ namespace UserService
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMiddleware<TokenMiddleware>();
 
             app.UseRouting();
 
---
+++ Services/UserService/Utils/TokenMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace UserService.Utils
+{
+    public class TokenMiddleware
+    {
+        private RequestDelegate next;
+
+        public TokenMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.Request.Method == "POST")
+            {
+                await next.Invoke(context);
+                return;
+            }
+            Console.WriteLine("1");
+
+            string token = context.Request.Query["token"];
+            string userId = context.Request.Query["userId"];
+
+            if (checkToken(userId, token))
+            {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync("Token is invalid");
+            }
+            else
+            {
+                await next.Invoke(context);
+            }
+        }
+
+        private bool checkToken(string userId, string token)
+        {
+            string ans;
+            using (var client = new HttpClient())
+            {
+                ans = client.GetStringAsync($"https://localhost:5001/Authentication/check?userId={userId}&token={token}").Result;
+            }
+
+            return ans == "true";
+        }
+
+
+    }
+}
