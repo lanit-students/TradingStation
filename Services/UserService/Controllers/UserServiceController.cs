3441602c687982ae9b77a834490c9215c2e9b2b7
dTarasova <d.al.tarasova@gmail.com>
2020-03-17 13:29:26 +0400

    Useless method is deleted

--- Services/UserService/Controllers/UserServiceController.cs
+++ Services/UserService/Controllers/UserServiceController.cs
@@ -14,17 +14,7 @@ namespace UserService.Controllers
         /// <summary>
         /// This method will be implemented in communication with other services
         /// </summary>
-        public string CreateUser([FromServices] string email, string password)
-        {
-            ICreateUser<string, string> command = new CreateUserCommand();
-            return command.Execute(email, password);
-        }
-
-        // This one is to test functionality of project 
-        // Add in the url - path "/UserService/get/email=will&password=work"
-        [Route("[controller]/get")]
-        [HttpGet]
-        public string TestCreateUserLogic([FromQuery] string email, string password)
+        public string CreateUser([FromQuery] string email, string password)
         {
             ICreateUser<string, string> command = new CreateUserCommand();
             return command.Execute(email, password);
