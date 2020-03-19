667a23464cbb9918e75ce3f9b29549008409c294
Dimentr <dima.zaharcev@mail.ru>
2020-03-19 23:38:17 +0300

    Add Command

---
+++ Services/UserService/Commands/DeleteUserCommand.cs
@@ -0,0 +1,24 @@
+﻿ using System;
+ using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+ using IDeleteUserUserService.Interfaces;
+ using Microsoft.AspNetCore.Mvc;
+
+ namespace UserService.Commands
+ 
+ {
+     public class DeleteUserCommand : IDeleteUser<Guid, string>
+     {
+         
+         public string Execute(Guid userId)
+         {
+             DeleteUserFromDataBaseService(userId);
+             return "Bravo";
+         }
+         private void DeleteUserFromDataBaseService(Guid userId)
+         {
+             return ;
+         }
+    }
+ }
