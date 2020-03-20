7c757465940df011ed028af4d163fb587e81b0d9
Dimentr <dima.zaharcev@mail.ru>
2020-03-21 01:30:08 +0300

    add controller

--- Services/UserService/Startup.cs
+++ Services/UserService/Startup.cs
@@ -1,8 +1,11 @@
+using System;
+using IDeleteUserUserService.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using UserService.Commands;
 
 namespace UserService
 {
@@ -12,6 +15,8 @@ namespace UserService
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+         
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
