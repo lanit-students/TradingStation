017e52f50e61c64bed16577f7f701aa64d2e8471
Dimentr <dima.zaharcev@mail.ru>
2020-03-20 15:06:03 +0300

    updated

--- Services/UserService/Commands/DeleteUserCommand.cs
+++ Services/UserService/Commands/DeleteUserCommand.cs
@@ -1,24 +1,30 @@
 ﻿ using System;
  using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+ using System.Net;
+ using System.Threading.Tasks;
  using IDeleteUserUserService.Interfaces;
  using Microsoft.AspNetCore.Mvc;
+ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
  namespace UserService.Commands
  
  {
      public class DeleteUserCommand : IDeleteUser<Guid, string>
      {
-         
+
          public string Execute(Guid userId)
          {
-             DeleteUserFromDataBaseService(userId);
-             return "Bravo";
-         }
-         private void DeleteUserFromDataBaseService(Guid userId)
+            if(DeleteUserFromDataBaseService(userId) == "Ok")
+            return "HttpStatusCode.OK";
+            else
+            {
+                return "400";
+            }
+        }
+         private string DeleteUserFromDataBaseService(Guid userId)
          {
-             return ;
+             return "Ok";
          }
     }
  }
