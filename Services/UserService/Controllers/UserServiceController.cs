a66a5536be83098461263acfc22657bf3958f421
Dimentr <dima.zaharcev@mail.ru>
2020-03-22 13:16:06 +0300

    delete unnecessary using

--- Services/UserService/Controllers/UserServiceController.cs
+++ Services/UserService/Controllers/UserServiceController.cs
@@ -1,26 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
-using IDeleteUserUserService.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Razor.Language.Intermediate;
-using UserService.Commands;
 
 namespace UserService.Controllers
 {
     [ApiController]
-
     public class UserServiceController : ControllerBase
     {
         [Route("[controller]/deleteUser")]
         [HttpGet]
         public string DeleteUserFromDataBaseService([FromQuery] Guid userId)
         {
-            IDeleteUser<Guid, string> deletecommand = new DeleteUserCommand();
-            return deletecommand.Execute(userId);
-
+            return "Hey";
+            //IDeleteUser<Guid, string> deletecommand =new DeleteUserCommand();
+            //return deletecommand.Execute(userId);
         }
     }
 }
