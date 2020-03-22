11f93ff654a1f345037befad85b99f3b44ccfba9
Дмитрий <39829236+Dimentr01@users.noreply.github.com>
2020-03-22 21:19:28 +0300

    Update DeleteUserCommand.cs

--- Services/UserService/Commands/DeleteUserCommand.cs
+++ Services/UserService/Commands/DeleteUserCommand.cs
@@ -1,20 +1,18 @@
-﻿ using System;
+ using System;
  using System.Net;
  using IDeleteUserUserService.Interfaces;
 
-
  namespace UserService.Commands
-
  {
-     public class DeleteUserCommand : IDeleteUser
+     public class DeleteUserCommand : IDeleteUserCommand
      {
-         public HttpStatusCode Execute(int userId)
+         public HttpStatusCode Execute(Guid userId)
          {
              return DeleteUserFromDataBaseService(userId) == "Ok" ? HttpStatusCode.OK : HttpStatusCode.BadRequest;
          }
 
-        //TODO fix from stub on request
-         private string DeleteUserFromDataBaseService(int userId)
+         //TODO fix from stub on request
+         private string DeleteUserFromDataBaseService(Guid userId)
          {
              return "Ok";
          }
