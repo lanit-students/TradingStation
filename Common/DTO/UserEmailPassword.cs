18b8a8388f7a8638e595de080ac88b00f3d4161b
Dmitry <st062458@student.spbu.ru>
2020-03-23 21:37:49 +0300

    Remove set

--- Common/DTO/UserEmailPassword.cs
+++ Common/DTO/UserEmailPassword.cs
@@ -6,7 +6,7 @@ namespace DTO
 {
     class UserEmailPassword
     {
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string Email { get; }
+        public string Password { get; }
     }
 }
