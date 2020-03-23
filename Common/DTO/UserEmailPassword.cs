e7e224aa3d207fa2a69ede6e50a15fe5ef9acb2f
Dmitry <st062458@student.spbu.ru>
2020-03-23 22:52:09 +0300

    Add constructor

--- Common/DTO/UserEmailPassword.cs
+++ Common/DTO/UserEmailPassword.cs
@@ -4,5 +4,11 @@
     {
         public string Email { get; }
         public string Password { get; }
+
+        public UserEmailPassword(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
     }
 }
