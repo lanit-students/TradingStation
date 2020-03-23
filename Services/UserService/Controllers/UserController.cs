72c13839b6944b726ab9b7226c07d12bcc3f55e8
dTarasova <d.al.tarasova@gmail.com>
2020-03-23 06:58:02 +0400

    small refactoring

--- Services/UserService/Controllers/UserController.cs
+++ Services/UserService/Controllers/UserController.cs
@@ -1,18 +1,14 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
-
 using IDeleteUserUserService.Interfaces;
 using DTO;
-using Microsoft.AspNetCore.Mvc;
 using System.Net;
-using UserService.Commands;
 using UserService.Interfaces;
 
 namespace UserService.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-<<<<<<< HEAD
     public class UserController : ControllerBase
     {
         
