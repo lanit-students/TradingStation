251a131a72b650cd6f7ed23390e9e22f0f44a235
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2020-03-24 13:43:18 +0300

    Create SignInModelValidation.cs

---
+++ Services/AuthenticationService/Validators/SignInModelValidation.cs
@@ -0,0 +1,23 @@
+﻿using DTO;
+using FluentValidation;
+
+namespace AuthenticationService.Validators
+{
+    public class SignInModelValidation : AbstractValidator<UserEmailPassword>
+    {
+        public SignInModelValidation()
+        {
+            RuleFor(user => user.Email)
+                .EmailAddress()
+                .WithMessage("Email address must be valid")
+                .NotEmpty()
+                .WithMessage("Email address must not be empty.");
+
+            RuleFor(user => user.Password)
+                .Length(5, 500)
+                .WithMessage("Password's length must be from 5 to 500 symbols")
+                .NotEmpty()
+                .WithMessage("Password must not be empty.");
+        }
+    }
+}
