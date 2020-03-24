946de8b7319a7e86442aac39f4797623deb6376c
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2020-03-24 11:53:38 +0300

    Added special view model

--- Clients/GUI/Startup.cs
+++ Clients/GUI/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Net.Http;
 
 namespace GUI
 {
---
+++ Clients/GUI/ViewModels/UserInfoViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GUI.ViewModels
+{
+    public class UserInfoViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string DateOfBirth { get; set; }
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
