29355e99fe53601ab960f3d5b5b8fb02b5fff510
dTarasova <d.al.tarasova@gmail.com>
2020-03-26 00:57:44 +0400

    Improve get function

--- Common/HttpWebRequestWrapperLib/HttpWebRequestWrapper.cs
+++ Common/HttpWebRequestWrapperLib/HttpWebRequestWrapper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Text;
@@ -10,31 +11,24 @@ namespace HttpWebRequestWrapperLib
         public HttpWebRequest httpWebRequest { get; set; }
         public HttpWebResponse httpWebResponse { get; set; }
 
-        public HttpWebRequestWrapper(string url, string contentType = "application/json")
+        public HttpWebRequestWrapper(string contentType = "application/json")
         {
-            httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
             httpWebRequest.ContentType = contentType;
         }
-
-        public string Get(string json)
+        
+        public string Get(string url, Dictionary<string,string> dictionary)
         {
+            if (dictionary != null || dictionary.Count < 1)
+            {
+                url = getUrlWithParams(url, dictionary);
+            }
             httpWebRequest.Method = "GET";
-            using (Stream requestStream = httpWebRequest.GetRequestStream())
-            {
-                using (StreamWriter streamWriter = new StreamWriter(requestStream))
-                {
-                    streamWriter.WriteLine(json);
-                }
-            }
+            HttpWebRequest.Create(url);
+
             httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-            string result;
-            using (Stream responseStream = httpWebResponse.GetResponseStream())
-            {
-                using (StreamReader streamReader = new StreamReader(responseStream, Encoding.UTF8))
-                {
-                    result = streamReader.ReadToEnd();
-                }
-            }
+            using Stream responseStream = httpWebResponse.GetResponseStream();
+            using StreamReader streamReader = new StreamReader(responseStream, Encoding.UTF8);
+            var result = streamReader.ReadToEnd();
             return result;
         }
 
@@ -53,6 +47,16 @@ namespace HttpWebRequestWrapperLib
             throw new NotImplementedException();
         }
 
-
+        private string getUrlWithParams(string url, Dictionary<string, string> dictionary)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(url + "?");
+            foreach (KeyValuePair<string, string> keyValuePair in dictionary)
+            {
+                sb.Append(keyValuePair.Key + "=" + keyValuePair.Value + "&");
+            }
+            sb.Remove(sb.Length - 1, 1);
+            return sb.ToString();
+        }
     }
 }
