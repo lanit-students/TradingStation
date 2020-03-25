3993cd238e07f8bc5dd504dc00c92ed63ee3d6e2
DanilFadeev <fdanya0@gmail.com>
2020-03-26 02:27:32 +0300

    add sign up page

---
+++ Clients/GUI/Scripts/SignUper.cs
@@ -0,0 +1,33 @@
+﻿using GUI.GUIModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using DTO;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace GUI.Scripts
+{
+    public class SignUper
+    {
+        public static HttpStatusCode SignUp(SignUpData insert)
+        {
+            UserEmailPassword output = new UserEmailPassword(insert.Email, insert.Password);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://localhost:5002/authentication/login");
+            request.Method = "POST";
+            string json = JsonSerializer.Serialize(insert);
+            request.ContentType = "application/json";
+            using (var dataStream = new StreamWriter(request.GetRequestStream()))
+            {
+                dataStream.Write(json);
+            }
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            return response.StatusCode;
+        }
+    }
+}
