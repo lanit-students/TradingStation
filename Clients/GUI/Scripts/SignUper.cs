9bb4575222df9087124e1cc8e408bf7a03045627
DanilFadeev <fdanya0@gmail.com>
2020-03-26 21:38:25 +0300

    remove unusing using

--- Clients/GUI/Scripts/SignUper.cs
+++ Clients/GUI/Scripts/SignUper.cs
@@ -1,12 +1,7 @@
 ﻿using GUI.GUIModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
 using DTO;
 using System.IO;
-using System.Text;
 using System.Text.Json;
 
 namespace GUI.Scripts
