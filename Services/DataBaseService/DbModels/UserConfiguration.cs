4467b28556d7b0005cc9b6fa7821e6411e5e78b2
Dmitry <st062458@student.spbu.ru>
2020-03-27 22:59:22 +0300

    Add UserConfiguration class

---
+++ Services/DataBaseService/DbModels/UserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataBaseService.DbModels
+{
+    public class UserConfigure : IEntityTypeConfiguration<DbUser>
+    {
+        public void Configure(EntityTypeBuilder<DbUser> builder)
+        {
+            builder.ToTable("Mobiles").HasKey(p => p.Id);
+            builder.Property(p => p.Email).IsRequired().HasMaxLength(30);
+        }
+    }
+}
