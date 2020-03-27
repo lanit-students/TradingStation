94b2e15b3de6caa01cecbc3796f65e74c6d8cebd
Dmitry <st062458@student.spbu.ru>
2020-03-27 16:00:54 +0300

    Spaces

--- Common/DTO/UserEmailPassword.cs
+++ Common/DTO/UserEmailPassword.cs
@@ -5,7 +5,7 @@
         public string Email { get; set; }
         public string Password { get; set; }
 
-        public UserEmailPassword() {    }
+        public UserEmailPassword() {}
 
         public UserEmailPassword(string email, string password)
         {
