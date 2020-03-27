d53f96f48d2b2dc0301ab24386f9a29e0e38eb87
Dmitry <st062458@student.spbu.ru>
2020-03-28 00:27:14 +0300

    Add UserMapper

---
+++ Services/DataBaseService/Mappers/UserMapper.cs
@@ -0,0 +1,23 @@
+﻿using DataBaseService.DbModels;
+using DataBaseService.Interfaces;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataBaseService.Mappers
+{
+    public class UserMapper : IMapper<UserEmailPassword, DbUser>
+    {
+        public DbUser CreateMap(UserEmailPassword data)
+        {
+            return new DbUser { Id = Guid.NewGuid(), Email = data.Email, Password = data.Password };
+        }
+
+        public UserEmailPassword CreateRemap(DbUser data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
