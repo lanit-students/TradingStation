dfeafc75d2aebc803269c94982ddabce88f03b5d
Dmitry <st062458@student.spbu.ru>
2020-03-28 00:27:39 +0300

    Add CreateUserCommand

---
+++ Services/DataBaseService/Commands/CreateUserCommand.cs
@@ -0,0 +1,24 @@
+﻿using DataBaseService.Interfaces;
+using DTO;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataBaseService.Commands
+{
+    public class CreateUserCommand : ICommand<UserEmailPassword>
+    {
+        private readonly IRepository<UserEmailPassword> userRepository;
+
+        public CreateUserCommand([FromServices] IRepository<UserEmailPassword> userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+        public void Execute(UserEmailPassword data)
+        {
+            userRepository.Create(data);
+        }
+    }
+}
