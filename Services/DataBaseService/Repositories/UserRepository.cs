5861c77475808c33f840685f4ce7fc52137aa766
Dmitry <st062458@student.spbu.ru>
2020-03-28 00:26:13 +0300

    Add UserRepository with create()

---
+++ Services/DataBaseService/Repositories/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using DataBaseService.DbModels;
+using DataBaseService.Interfaces;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataBaseService.Repositories
+{
+    public class UserRepository : IRepository<UserEmailPassword>
+    {
+        readonly IMapper<UserEmailPassword, DbUser> mapper;
+        readonly DataBaseContext dbContext;        
+        
+        public UserRepository(IMapper<UserEmailPassword, DbUser> mapper, DataBaseContext dbContext)
+        {
+            this.mapper = mapper;
+            this.dbContext = dbContext;
+        }
+        public void Create(UserEmailPassword data)
+        {
+            dbContext.Add(mapper.CreateMap(data));
+            dbContext.SaveChanges();
+        }
+
+    }
+}
+
