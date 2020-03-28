6b6e57c565bc0b13b3346889dd0a8b63c524bfe7
Dmitry <st062458@student.spbu.ru>
2020-03-28 23:21:43 +0300

    Add configuration for DbUser

--- Services/DataBaseService/DbModels/DbUser.cs
+++ Services/DataBaseService/DbModels/DbUser.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 
 namespace DataBaseService.DbModels
 {
@@ -7,6 +9,16 @@ namespace DataBaseService.DbModels
         public Guid Id { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+    }
 
+    public class UserConfiguration : IEntityTypeConfiguration<DbUser>
+    {
+        public void Configure(EntityTypeBuilder<DbUser> builder)
+        {
+            builder.ToTable("Users").HasKey(p => p.Id);
+            builder.Property(p => p.Id).HasColumnName("Email").IsRequired().HasMaxLength(30);
+            builder.Property(p => p.Email).HasColumnName("Email").IsRequired().HasMaxLength(30);
+            builder.Property(p => p.Password).HasColumnName("Password").IsRequired().HasMaxLength(25);
+        }
     }
 }
