92691f9ee93acfb4ba42ee8947220d596a9a9d60
Dmitry <st062458@student.spbu.ru>
2020-03-29 21:31:54 +0300

    fixed script

--- Services/DataBaseService/Utils/MigrationScripts/24.03.2020/0001_CreateUsersTable.sql
+++ Services/DataBaseService/Utils/MigrationScripts/24.03.2020/0001_CreateUsersTable.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [UsersCredentials] (
     [Id]		    UNIQUEIDENTIFIER NOT NULL,
     [Email]		    NVARCHAR (30)    NOT NULL,
-    [PasswordHash]  INTEGER          NOT NULL,
+    [PasswordHash]  NVARCHAR (40)         NOT NULL,
     CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
