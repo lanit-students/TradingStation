317a9be2651f676dc08ea939193c1556b23e8e65
dTarasova <d.al.tarasova@gmail.com>
2020-04-01 13:04:27 +0400

    Check type of object

--- Tests/NewsServiceTests/Utils/NewsPublisherFactoryTests.cs
+++ Tests/NewsServiceTests/Utils/NewsPublisherFactoryTests.cs
@@ -1,23 +1,25 @@
-﻿
+﻿using Interfaces;
 using NewsService;
 using NewsService.Utils;
 using NUnit.Framework;
-
+using CentralBankIntegration;
 namespace NewsServiceTests.Utils
 {
     class NewsPublisherFactoryTests
     {
-        private NewsPublisherTypes centralBankNewsPublisher = NewsPublisherTypes.CentralBank;
+        private NewsPublisherTypes centralBankNewsPublisherType = NewsPublisherTypes.CentralBank;
+        private INewsPublisher centralBankNewsPublisher;
 
         [SetUp]
         public void Initialize()
         {
+            centralBankNewsPublisher = new CentralBankNewsPublisher();
         }
 
         [Test]
         public void CreateCBNewsPublisherTest()
         {
-            Assert.NotNull(NewsPublisherFactory.Create(centralBankNewsPublisher));
+            Assert.AreEqual((NewsPublisherFactory.Create(centralBankNewsPublisherType)).GetType(), centralBankNewsPublisher.GetType());
         }
     }
 }
