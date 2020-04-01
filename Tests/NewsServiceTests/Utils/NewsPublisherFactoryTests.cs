f03bbe0383511f426d34b5f1f3e42f8339361930
dTarasova <d.al.tarasova@gmail.com>
2020-04-01 12:51:02 +0400

    CB test created

--- Tests/NewsServiceTests/Utils/NewsPublisherFactoryTests.cs
+++ Tests/NewsServiceTests/Utils/NewsPublisherFactoryTests.cs
@@ -1,8 +1,23 @@
 ﻿
+using NewsService;
+using NewsService.Utils;
+using NUnit.Framework;
 
 namespace NewsServiceTests.Utils
 {
     class NewsPublisherFactoryTests
     {
+        private NewsPublisherTypes centralBankNewsPublisher = NewsPublisherTypes.CentralBank;
+
+        [SetUp]
+        public void Initialize()
+        {
+        }
+
+        [Test]
+        public void CreateCBNewsPublisherTest()
+        {
+            Assert.NotNull(NewsPublisherFactory.Create(centralBankNewsPublisher));
+        }
     }
 }
