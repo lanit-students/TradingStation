694b645be8323cde5f2347931887c06e87921a7d
DanilFadeev <fdanya0@gmail.com>
2020-04-01 21:51:35 +0300

    using http wrapper

--- Clients/GUI/Scripts/SignUper.cs
+++ Clients/GUI/Scripts/SignUper.cs
@@ -1,33 +1,18 @@
 using GUI.GUIModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
 using DTO;
 using System.IO;
-using System.Text;
 using System.Text.Json;
+using HttpWebRequestWrapperLib;
 
 namespace GUI.Scripts
 {
-    public class SignUper
+    public class SignUpper
     {
-        public static HttpStatusCode SignUp(SignUpViewModel insert)
+        public static void SignUp(SignUpViewModel input)
         {
-            var output = new UserEmailPassword(insert.Email, insert.Password);
-
-            var request = (HttpWebRequest)WebRequest.Create("https://localhost:5011/user/create");
-            request.Method = "POST";
-            string json = JsonSerializer.Serialize(output);
-            request.ContentType = "application/json";
-            using (var dataStream = new StreamWriter(request.GetRequestStream()))
-            {
-                dataStream.Write(json);
-            }
-
-            var response = (HttpWebResponse)request.GetResponse();
-            return response.StatusCode;
+            var output = new UserEmailPassword(input.Email, input.Password);
+            new HttpWebRequestWrapper().Post("https://localhost:5011/user/create", null, output);
         }
     }
 }
