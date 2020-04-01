8ba2f75b19d7e32f2b2fe2b2eec5e21dec7433a4
Denis <den89181827071@mail.ru>
2020-04-01 14:10:44 +0300

    add signIner

--- Clients/GUI/Scripts/SignIner.cs
+++ Clients/GUI/Scripts/SignIner.cs
@@ -1,11 +1,30 @@
-﻿using System;
+﻿using DTO;
+using HttpWebRequestWrapperLib;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace GUI.Scripts
 {
     public class SignIner
     {
+        public static string SignIn(UserEmailPassword userInput)
+        {
+            var requestWrapper = new HttpWebRequestWrapper();
+            var result = "";
+            try
+            {
+                result = requestWrapper.Post("http://localhost:5001/authentication/login", null, userInput);
+            }
+            catch(Exception e)
+            {
+                return e.Message;
+            }
+            return result;
+        }
     }
 }
