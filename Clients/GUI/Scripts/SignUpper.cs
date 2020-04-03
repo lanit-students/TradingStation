46b5df24682b828d56d3ac79fc8619ace1e2e295
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2020-04-03 10:57:14 +0300

    Fixed SignUp parameter

--- Clients/GUI/Scripts/SignUpper.cs
+++ Clients/GUI/Scripts/SignUpper.cs
@@ -6,10 +6,9 @@ namespace GUI.Scripts
 {
     public class SignUpper
     {
-        public static void SignUp(SignUpViewModel input)
+        public static void SignUp(UserEmailPassword data)
         {
-            var output = new UserEmailPassword(input.Email, input.Password);
-            new HttpWebRequestWrapper().Post("https://localhost:5011/user/create", null, output);
+            new HttpWebRequestWrapper().Post("https://localhost:5011/user/create", null, data);
         }
     }
 }
