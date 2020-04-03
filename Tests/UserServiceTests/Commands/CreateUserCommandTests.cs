6a85201937775ae3dd4137e62eeba56431bc854d
dTarasova <d.al.tarasova@gmail.com>
2020-04-03 23:05:24 +0400

    UserCommandTests created

---
+++ Tests/UserServiceTests/Commands/CreateUserCommandTests.cs
@@ -0,0 +1,29 @@
+﻿using DTO;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserServiceTests.Commands
+{
+    public class CreateUserCommandTests
+    {
+        private UserEmailPassword nullUser;
+        private UserEmailPassword goodUser;
+        private UserEmailPassword badUser;
+
+
+        [SetUp]
+        public void Initialize()
+        {
+
+        }
+
+        [Test]
+        public void Test()
+        {
+
+        }
+
+    }
+}
