3e8723abcc9fe265b24bc66bf2387cdf3cfc43b0
Leonid <konoplin.7777@gmail.com>
2020-04-05 12:18:10 +0300

    Front complete

--- Services/NewsService/Controllers/NewsController.cs
+++ Services/NewsService/Controllers/NewsController.cs
@@ -26,25 +26,18 @@ namespace NewsService.Controllers
         /// </summary>
         [Route("currencies")]
         [HttpPost]
-        public string GetCurrencies(
-            //[FromBody] CurrencyRequest requestParams,
+        public List<ExchangeRate> GetCurrencies(
+            [FromBody] CurrencyRequest requestParams,
             [FromServices] IValidator<CurrencyRequest> validator)
         {
-            CurrencyRequest requestParams = new CurrencyRequest
-            {
-                CurrencyCodes = new List<string> { "eur", "usd" },
-                CurrecyPublisher = NewsPublisherTypes.CentralBank
-            };
-
             validator.ValidateAndThrow(requestParams);
             List<ExchangeRate> rates = NewsPublisherFactory
                 .Create(requestParams.CurrecyPublisher)
                 .GetCurrencies();
 
-            return "hhh";
-                //rates
-                //.Where(r => requestParams.CurrencyCodes.Contains(r.Code))
-                //.ToList();
+            return rates
+                .Where(r => requestParams.CurrencyCodes.Contains(r.Code))
+                .ToList();
         }
     }
 }
