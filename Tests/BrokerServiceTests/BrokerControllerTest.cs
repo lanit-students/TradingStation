e6ac286b288a842e036da9908e96c06f95344910
Maksim Panasenko <maks.panasenko01@bk.ru>
2020-04-06 20:44:18 +0300

    Added a test class BrokerControllerTest

---
+++ Tests/BrokerServiceTests/BrokerControllerTest.cs
@@ -0,0 +1,49 @@
+using NUnit.Framework;
+using Interfaces;
+using BrokerService.Controllers;
+using System;
+
+namespace BrokerServiceTests
+{
+    [TestFixture]
+    public class BrokerControllerTests
+    {
+        private BrokerController brokerController;
+        [SetUp]
+        public void Setup()
+        {
+             brokerController = new BrokerController(); 
+        }
+
+        [Test]
+        public void GetCurrency_ReturnAggregateException()
+        {
+            Assert.Throws<AggregateException>(() => brokerController.GetCurrency(BrokerServices.BankType.TinkoffBank, "wrong string"));
+        }
+        [Test]
+        public void GetAllCurrencies_ReturnAggregateException()
+        {
+            Assert.Throws<AggregateException>(() => brokerController.GetAllCurrencies(BrokerServices.BankType.TinkoffBank));
+        }
+        [Test]
+        public void GetAllStocks_ReturnAggregateException()
+        {
+            Assert.Throws<AggregateException>(() => brokerController.GetAllStocks(BrokerServices.BankType.TinkoffBank));
+        }
+        [Test]
+        public void GetStock_ReturnAggregateException()
+        {
+            Assert.Throws<AggregateException>(() => brokerController.GetStock(BrokerServices.BankType.TinkoffBank,"wrong string"));
+        }
+        [Test]
+        public void GetAllBonds_ReturnAggregateException()
+        {
+            Assert.Throws<AggregateException>(() => brokerController.GetAllBonds(BrokerServices.BankType.TinkoffBank));
+        }
+        [Test]
+        public void GetBond_ReturnAggregateException()
+        {
+            Assert.Throws<AggregateException>(() => brokerController.GetBond(BrokerServices.BankType.TinkoffBank, "wrong string"));
+        }
+    }
+}
