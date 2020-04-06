da54a392e5f540c84a20505dd87aeb4712ba1386
Dmitry <st062458@student.spbu.ru>
2020-04-06 14:57:37 +0300

    Remove checks and fix conflicts

--- Services/DataBaseService/Repositories/UserRepository.cs
+++ Services/DataBaseService/Repositories/UserRepository.cs
@@ -28,47 +28,22 @@ namespace DataBaseService.Repositories
 
         public void CreateUserCredential(UserCredential userCredential)
         {
-            var dbCredential = dbContext.UsersCredentials
-                .FirstOrDefault(uc => uc.Email == userCredential.Email);
-
-            if (dbCredential == null)
-            {
-                throw new BadRequestException("User already exist");
-            }
-            else
-            {
-                dbContext.UsersCredentials.Add(mapper.MapToDbUserCredential(userCredential));
-                dbContext.SaveChanges();
-            }
+            dbContext.UsersCredentials.Add(mapper.MapToDbUserCredential(userCredential));
+            dbContext.SaveChanges();
         }
 
         public UserCredential GetUserCredential(string email)
         {
             var dbCredential = dbContext.UsersCredentials.FirstOrDefault(uc => uc.Email == email);
-            if (dbCredential == null)
-            {
-                throw new NotFoundException("User not found");
-            }
 
             return mapper.MapUserCredential(dbCredential);
         }
-
-        public User GetUserById(Guid userId)
+        public void DeleteUser(Guid userIdCredential)
         {
-            var dbUser = dbContext.Users.FirstOrDefault(uc => uc.Id == userId);
-            if (dbUser == null)
-            {
-                throw new NotFoundException("User not found");
-            }
-            return mapper.MapUser(dbUser);
-        }
-
-        public void DeleteUser(Guid userId)
-        {
-            var dbUserCredential = dbContext.Find<DbUserCredential>(userId);
+            var dbUserCredential = dbContext.Find<DbUserCredential>(userIdCredential);
             if (dbUserCredential == null)
             {
-                throw new NotFoundException("Not found User for delete");
+                throw new ForbiddenException("Not found User for delete");
             }
             if(dbUserCredential.IsActive==false)
             {
@@ -79,4 +54,3 @@ namespace DataBaseService.Repositories
         }
     }
 }
-
