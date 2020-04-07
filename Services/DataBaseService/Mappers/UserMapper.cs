a960a1be547d19fda72e46bfd9e6db82cfbc9efd
Leonid <konoplin.7777@gmail.com>
2020-04-07 23:22:24 +0300

    small ref

--- Services/DataBaseService/Mappers/UserMapper.cs
+++ Services/DataBaseService/Mappers/UserMapper.cs
@@ -1,7 +1,6 @@
-﻿using DataBaseService.Database.Models;
+﻿using DTO;
+using DataBaseService.Database.Models;
 using DataBaseService.Mappers.Interfaces;
-using DTO;
-using System;
 
 namespace DataBaseService.Mappers
 {
