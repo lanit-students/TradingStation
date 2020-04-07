9c6f554f461632c28e10240f6aba6d03340720c7
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2020-04-07 20:06:15 +0300

    Update UserRepository.cs

--- Services/DataBaseService/Repositories/UserRepository.cs
+++ Services/DataBaseService/Repositories/UserRepository.cs
@@ -54,16 +54,19 @@ namespace DataBaseService.Repositories
         public void DeleteUser(Guid userId)
         {
             var dbUserCredential = dbContext.UsersCredentials.FirstOrDefault(uc => uc.UserId == userId);
-            if (dbUserCredential == null)
+
+            if (dbUserCredential is null)
             {
-                throw new ForbiddenException("Not found User for delete");
+                throw new NotFoundException("Not found User for delete");
             }
-            if(dbUserCredential.IsActive==false)
+
+            if(!dbUserCredential.IsActive)
             {
-                throw new ForbiddenException("User was deleted early or not confirmed");
+                throw new BadRequestException("User was deleted early or not confirmed");
             }
-             dbUserCredential.IsActive = false;
-             dbContext.SaveChanges();
+
+            dbUserCredential.IsActive = false;
+            dbContext.SaveChanges();
         }
     }
 }
