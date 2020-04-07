1466b2132e3e66fbee23138710ab6293043fd375
VLiamin <LyaminVA@yandex.ru>
2020-04-07 18:11:29 +0300

    desired parameter deleted

--- Services/UserService/Commands/EditUserCommand.cs
+++ Services/UserService/Commands/EditUserCommand.cs
@@ -1,5 +1,10 @@
-﻿using DTO.RestRequests;
+﻿using DTO;
+using DTO.BrokerRequests;
+using DTO.RestRequests;
 using FluentValidation;
+using Kernel;
+using Kernel.CustomExceptions;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Threading.Tasks;
@@ -11,29 +16,78 @@ namespace UserService.Commands
     {
         private readonly IValidator<UserInfoRequest> userInfoValidator;
         private readonly IValidator<PasswordChangeRequest> passwordChangeValidator;
-        
+        private readonly IBus busControl;
+
         public EditUserCommand
-            ([FromServices] IValidator<UserInfoRequest> userInfoValidator, 
-            [FromServices] IValidator<PasswordChangeRequest> passwordChangeValidator)
+            ([FromServices] IValidator<UserInfoRequest> userInfoValidator,
+            [FromServices] IValidator<PasswordChangeRequest> passwordChangeValidator,
+            [FromServices] IBus busControl)
         {
+            this.busControl = busControl;
             this.userInfoValidator = userInfoValidator;
             this.passwordChangeValidator = passwordChangeValidator;
         }
-        
-        public Task<bool> Execute(EditUserRequest request)
+
+        public async Task<bool> Execute(EditUserRequest request)
         {
             var passwordRequest = request.PasswordRequest;
             var userInfo = request.UserInfo;
-            
-            if (!(string.IsNullOrEmpty(passwordRequest.OldPassword)
-                    || string.IsNullOrEmpty(passwordRequest.NewPassword)))
+            try
             {
-                passwordChangeValidator.ValidateAndThrow(passwordRequest);
+                if (!(string.IsNullOrEmpty(passwordRequest.OldPassword)
+                        || string.IsNullOrEmpty(passwordRequest.NewPassword)))
+                {
+                    passwordChangeValidator.ValidateAndThrow(passwordRequest);
+                }
             }
+            catch
+            {
+                throw new BadRequestException("Unable to edit");
+            }
+
             userInfoValidator.ValidateAndThrow(userInfo);
 
-            // TODO Realise edit user logic
-            throw new NotImplementedException();
+            string oldPasswordHash = ShaHash.GetPasswordHash(passwordRequest.OldPassword);
+            string newPasswordHash = ShaHash.GetPasswordHash(passwordRequest.NewPassword);
+            var user = new User
+            {
+                Id = userInfo.UserId,
+                Birthday = userInfo.Birthday,
+                FirstName = userInfo.FirstName,
+                LastName = userInfo.LastName,
+                Email = userInfo.Email
+            };
+
+            var passwordHashChangeRequest = new PasswordHashChangeRequest
+            {
+                OldPasswordHash = oldPasswordHash,
+                NewPasswordHash = newPasswordHash
+            };
+
+            var internalEditUserInfoRequest = new InternalEditUserInfoRequest
+            {
+                User = user,
+                UserPasswords = passwordHashChangeRequest
+            };
+
+            var editUserResult = await EditUser(internalEditUserInfoRequest);
+            if (!editUserResult)
+            {
+                throw new BadRequestException("Unable to edit");
+            }
+
+            return editUserResult;
+        }
+
+        private async Task<bool> EditUser(InternalEditUserInfoRequest request)
+        {
+            var uri = new Uri("rabbitmq://localhost/DatabaseService");
+
+            var client = busControl.CreateRequestClient<InternalEditUserInfoRequest>(uri).Create(request);
+
+            var response = await client.GetResponse<OperationResult>();
+
+            return response.Message.IsSuccess;
         }
     }
 }
