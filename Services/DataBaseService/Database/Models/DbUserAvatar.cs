2df74fbabc85774f94e6de8d34b46f534f562b0b
ifelseelif <yougrysha@gmail.com>
2020-04-10 02:52:09 +0300

    added DbUserAvatar

---
+++ Services/DataBaseService/Database/Models/DbUserAvatar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataBaseService.Database.Models
+{
+    public class DbUserAvatar
+    {
+        public Guid Id { get; set; }
+        public byte[] Avatar { get; set; }
+        public string TypeAvatar { get; set; }
+    }
+
+    public class DbUserAvatarConfiguration : IEntityTypeConfiguration<DbUserAvatar>
+    {
+        public void Configure(EntityTypeBuilder<DbUserAvatar> builder)
+        {
+            builder
+                .ToTable("UserAvatars")
+                .HasKey(p => p.Id);
+            builder
+                .Property(p => p.Id)
+                .HasColumnName("Id")
+                .IsRequired();
+            builder
+                .Property(p => p.Avatar)
+                .HasColumnName("Avatar")
+                .IsRequired();
+            builder
+                .Property(p => p.TypeAvatar)
+                .HasColumnName("TypeAvatar")
+                .IsRequired();
+        }
+    }
+}
