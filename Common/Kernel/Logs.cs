2680c8a2dad269dd5fdd4a2abe20ff467aaefd7a
Dmitry <st062458@student.spbu.ru>
2020-04-11 02:48:29 +0300

    Add logs model in kernel

---
+++ Common/Kernel/Logs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kernel
+{
+    class Logs
+    {
+        public Guid Id { get; set; }
+        public Guid ParentId { get; set; }
+        public string Type { get; set; }
+        public DateTime Time { get; set; }
+        public string Message { get; set; }
+        public string ServiceName { get; set; }
+    }
+}
