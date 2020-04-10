38c0bca21ace1bd6d34bdc2927b5c0e3aef92d23
ifelseelif <yougrysha@gmail.com>
2020-04-10 18:22:50 +0300

    added validation parameter

---
+++ Clients/GUI/CustomValidationAtributes/BirthdayAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using GUI.ViewModels;
+
+namespace GUI.CustomValidationAttributes
+{
+    public class BirthdayAttribute : ValidationAttribute
+    {
+        public string GetErrorMessageTooYoung() =>
+            $"You are to young.";
+
+        protected override ValidationResult IsValid(object value,
+            ValidationContext validationContext)
+        {
+            var birthday = (DateTime) value;
+            
+            if (birthday.AddYears(18) > DateTime.Now)
+            {
+                return new ValidationResult(GetErrorMessageTooYoung());
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ Clients/GUI/CustomValidationAtributes/NameValidation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using GUI.ViewModels;
+
+namespace GUI.CustomValidationAtributes
+{
+    public class NameValidation : ValidationAttribute
+    {
+        public string GetErrorMessageNoNumbers() =>
+            $"Not allow number";
+        public string GetErrorMessageTooShort() =>
+            $"name too short";
+
+        public string GetErrorMessage() =>
+            $"The first character must be a capital letter, the rest are small";
+
+
+        protected override ValidationResult IsValid(object value,
+            ValidationContext validationContext)
+        {
+            var name = (String) value;
+
+            if (!name.All(char.IsLetter))
+            {
+                return new ValidationResult(GetErrorMessageNoNumbers());
+            }
+
+            if (name.Length < 2)
+            {
+                return new ValidationResult(GetErrorMessageTooShort());
+            }
+
+            if ( !char.IsUpper(name[0]) || !name.Substring(1).All(char.IsLower))
+            {
+                return new ValidationResult(GetErrorMessage());
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
