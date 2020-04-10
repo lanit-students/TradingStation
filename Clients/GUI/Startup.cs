bbed0c2052d4d18a61c3c6692a974d06bf87c003
ifelseelif <yougrysha@gmail.com>
2020-04-10 19:13:08 +0300

    added singupwizzard

--- Clients/GUI/Startup.cs
+++ Clients/GUI/Startup.cs
@@ -32,6 +32,11 @@ namespace GUI
             services.AddScoped<AuthStateProvider>();
             services.AddScoped<AuthenticationStateProvider>(
                     provider => provider.GetRequiredService<AuthStateProvider>());
+
+            services.AddServerSideBlazor().AddHubOptions(o =>
+            {
+                o.MaximumReceiveMessageSize = 10 * 1024 * 1024; // 10MB
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- Clients/GUI/ViewModels/SignUpViewModel.cs
+++ Clients/GUI/ViewModels/SignUpViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using GUI.CustomValidationAtributes;
+using GUI.CustomValidationAttributes;
 
 namespace GUI.ViewModels
 {
@@ -9,13 +11,17 @@ namespace GUI.ViewModels
     public class SignUpViewModel
     {
         [Required(ErrorMessage = "Name is required")]
+        [StringLength(32, ErrorMessage = "First name is too long.")]
+        [NameValidation]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Surname is required")]
-        [StringLength(32, ErrorMessage = "LastName is too long.")]
+        [StringLength(32, ErrorMessage = "Last name is too long.")]
+        [NameValidation]
         public string LastName { get; set; }
 
         [Required(ErrorMessage = "Date of birth is required")]
+        [BirthdayAttribute]
         public DateTime Birthday { get; set; }
 
         [Required(ErrorMessage = "Email is required")]
@@ -27,7 +33,8 @@ namespace GUI.ViewModels
 
         [Compare(nameof(Password))]
         public string ConfirmPassword { get; set; }
-
+        public byte[] Avatar { get; set; }
+        public string TypeAvatar { get; set; }
         public SignUpViewModel()
         {
             Birthday = DateTime.Today.AddYears(-18);
