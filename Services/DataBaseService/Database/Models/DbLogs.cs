c10b3a67544837653d888a7b7c57de6b0485df1d
Dmitry <st062458@student.spbu.ru>
2020-04-11 02:17:47 +0300

    Add context and model

---
+++ Services/DataBaseService/Database/Models/DbLogs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataBaseService.Database.Models
+{
+    public class DbLogs
+    {
+    }
+}
