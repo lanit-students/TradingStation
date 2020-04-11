9075af26c2c041d8d6dd950b683f35a299590b63
Dmitry <st062458@student.spbu.ru>
2020-04-11 06:09:26 +0300

    Add log consumer

---
+++ Services/DataBaseService/BrokerConsumers/AddLogConsumer.cs
@@ -0,0 +1,36 @@
+﻿using DataBaseService.Database.Logs.Interfaces;
+using DTO;
+using Kernel;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DataBaseService.BrokerConsumers
+{
+    public class AddLogConsumer : IConsumer<Log>
+    {
+        private readonly ILogRepository logRepository;
+
+        public AddLogConsumer([FromServices] ILogRepository logRepository)
+        {
+            this.logRepository = logRepository;
+        }
+
+        private OperationResult AddLog(Log log)
+        {
+            logRepository.AddLogs(log);
+
+            return new OperationResult
+            {
+                IsSuccess = true
+            };
+        }
+
+        public async Task Consume(ConsumeContext<Log> context)
+        {
+            var addResult = AddLog(context.Message);
+
+            await context.RespondAsync(addResult);
+        }
+    }
+}
