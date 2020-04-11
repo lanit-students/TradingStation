98c291a9e0fc40c2e89b69100c7c8764b36d319b
Dmitry <st062458@student.spbu.ru>
2020-04-11 06:09:00 +0300

    Some fixes in DbLog and context

--- Services/DataBaseService/Database/Logs/TPlatformLogDbContext.cs
+++ Services/DataBaseService/Database/Logs/TPlatformLogDbContext.cs
@@ -6,11 +6,11 @@ using DataBaseService.Database.Models;
 
 namespace DataBaseService.Database
 {
-    public class TPlatformLogsDbContext : DbContext
+    public class TPlatformLogDbContext : DbContext
     {
         public DbSet<DbLog> Logs { get; set; }
 
-        public TPlatformLogsDbContext(DbContextOptions<TPlatformLogsDbContext> options) : base(options) { }
+        public TPlatformLogDbContext(DbContextOptions<TPlatformLogDbContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
