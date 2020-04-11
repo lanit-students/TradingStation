7a9764ac22e63b15e566d71d376d91f855625bc6
Denis <den89181827071@mail.ru>
2020-04-11 15:38:12 +0300

    add class constants

---
+++ Services/BrokerService/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace BrokerService
+{
+    static class Constants
+    {
+        internal static int Depth = 5;
+        public static string Token = "t.-lW-yXwRofmPWDrSqm9XpBO2K4WbTmHPkKPgRskRDlMqLCxzbP8efGmM3l6U1HU9ReMg5R0t3QqO86twfG3-vw";
+    }
+}
