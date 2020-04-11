4efadcb3b168e8c064f1ffc64db8807fdf3da21f
Dmitry <st062458@student.spbu.ru>
2020-04-11 15:42:43 +0300

    First trying

--- Common/Kernel/LoggingEngine/Logger.cs
+++ Common/Kernel/LoggingEngine/Logger.cs
@@ -1,10 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
 
 namespace Kernel.LoggingEngine
 {
-    class Logger
+    public class Logger: ILogger
     {
+        private readonly IBus busControl;
+        private static object _lock = new object();
+
+        public Logger(IBus busControl)
+        {
+            this.busControl = busControl;
+        }
+
+        private async Task<Log> AddLog(Log log)
+        {
+            var uri = new Uri("rabbitmq://localhost/DatabaseService");
+
+            var client = busControl.CreateRequestClient<Log>(uri).Create(log);
+
+            var response = await client.GetResponse<Log>();
+
+            return response.Message;
+        }
+
+        public async Task<Log> Execute(Log log)
+        {
+            var user = await AddLog(log);
+
+            return user;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            //return logLevel == LogLevel.Trace;
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            if (formatter != null)
+            {
+                lock (_lock)
+                {                    
+                }
+            }
+        }
     }
 }
--- Common/Kernel/LoggingEngine/LoggerProvider.cs
+++ Common/Kernel/LoggingEngine/LoggerProvider.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Kernel.LoggingEngine
 {
-    class FileLoggerProvider
+    public class LoggerProvider: ILoggerProvider
     {
+        private readonly IBus busControl;
+        public LoggerProvider(string _path)
+        {
+        }
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new Logger(busControl);
+        }
+
+        public void Dispose()
+        {
+        }
     }
 }
