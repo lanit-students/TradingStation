3aa0d44d1d76029b952e7b467b7ffc1774373ea7
Maksim Panasenko <maks.panasenko01@bk.ru>
2020-04-11 15:31:58 +0300

    Change BrokerFactory

--- Services/BrokerService/Utils/BrokerFactory.cs
+++ Services/BrokerService/Utils/BrokerFactory.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using BrokerService;
+using DTO;
 using Interfaces;
+using Kernel.CustomExceptions;
+using TinkoffIntegrationLib;
 
 namespace BrokerServices.Utils
 {
-    public static class BrokerFactory
+    static class BrokerFactory
     {
-        public static IBroker Create(BankType bankType)
+        internal static IBroker Create(BankType bankType)
         {
-            throw new NotImplementedException();
+            var brokerData = new CreateBrokerData
+            {
+                Depth = Constants.Depth,
+                Token = Constants.Token
+            };
+            if (bankType == BankType.TinkoffBank) return new TinkoffBankBroker(brokerData);
+            throw new BadRequestException("We can't do it with this bank.");
         }
     }
 }
