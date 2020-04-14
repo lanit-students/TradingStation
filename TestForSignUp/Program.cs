42f610617473f81cf0541196aa71f84984db7588
Denis <den89181827071@mail.ru>
2020-04-14 19:11:47 +0300

    add a template

---
+++ TestForSignUp/Program.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+
+namespace TestForSignUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            browser.Navigate().GoToUrl("");
+
+        }
+    }
+}
