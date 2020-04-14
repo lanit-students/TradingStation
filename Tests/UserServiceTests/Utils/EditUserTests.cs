b791bd01b4d892c05fdd21ca25a1b2c0fe79fbed
VLiamin <LyaminVA@yandex.ru>
2020-04-14 14:47:01 +0300

    added test with exception call

--- Tests/UserServiceTests/Utils/EditUserTests.cs
+++ Tests/UserServiceTests/Utils/EditUserTests.cs
@@ -3,6 +3,7 @@ using DTO.BrokerRequests;
 using DTO.RestRequests;
 using FluentValidation;
 using FluentValidation.Results;
+using Kernel.CustomExceptions;
 using MassTransit;
 using Moq;
 using NUnit.Framework;
@@ -22,35 +23,34 @@ namespace UserServiceTests
         private Mock<IRequestClient<InternalEditUserInfoRequest>> requestClientMock;
         private Mock<Response<OperationResult>> resultMock;
         private IEditUserCommand command;
+        private UserInfoRequest infoRequest;
 
         [SetUp]
         public void Init()
         {
-            OperationResult operation = new OperationResult();
-            operation.IsSuccess = true;
             resultMock = new Mock<Response<OperationResult>>();
             resultMock
                 .Setup(x => x.Message)
-                .Returns(operation);
+                .Returns(new OperationResult() {IsSuccess = true});
+
             requestClientMock = new Mock<IRequestClient<InternalEditUserInfoRequest>>();
             requestClientMock
                 .Setup(x => x.GetResponse<OperationResult>(It.IsAny<InternalEditUserInfoRequest>(), default, default))
                 .Returns(Task.FromResult(resultMock.Object));
+
             userValidatorMock = new Mock<IValidator<UserInfoRequest>>();
             userValidatorMock
                 .Setup(x => x.Validate(It.IsAny<ValidationContext>()))
                 .Returns(new ValidationResult());
+
             passwordValidatorMock = new Mock<IValidator<PasswordChangeRequest>>();
             passwordValidatorMock
                 .Setup(x => x.Validate(It.IsAny<ValidationContext>()))
                 .Returns(new ValidationResult());
-            command = new EditUserCommand(userValidatorMock.Object, passwordValidatorMock.Object, requestClientMock.Object);
-        }
 
-        [Test]
-        public void TestWithAllOptions()
-        {
-            var infoRequest = new UserInfoRequest
+            command = new EditUserCommand(userValidatorMock.Object, passwordValidatorMock.Object, requestClientMock.Object);
+
+            infoRequest = new UserInfoRequest
             {
                 UserId = Guid.NewGuid(),
                 Email = "bla.bla.com",
@@ -58,7 +58,11 @@ namespace UserServiceTests
                 LastName = "Kor",
                 Birthday = DateTime.Today.AddYears(-20)
             };
+        }
 
+        [Test]
+        public void TestWithAllOptions()
+        { 
             var passwordRequest = new PasswordChangeRequest
             {
                 OldPassword = "oldPassword",
@@ -75,15 +79,6 @@ namespace UserServiceTests
         [Test]
         public void TestWithNullPassword()
         {
-            var infoRequest = new UserInfoRequest
-            {
-                UserId = Guid.NewGuid(),
-                Email = "bla.bla.com",
-                FirstName = "Leo",
-                LastName = "Kor",
-                Birthday = DateTime.Today.AddYears(-20)
-            };
-
             var request = new EditUserRequest
             {
                 PasswordRequest = null,
@@ -91,6 +86,24 @@ namespace UserServiceTests
             };
 
             Assert.IsTrue(command.Execute(request).Result);
+        }
+
+        [Test]
+        public void TestWithErrorPasswordOptions()
+        {
+            var passwordRequest = new PasswordChangeRequest
+            {
+                OldPassword = null,
+                NewPassword = "newPassword",
+            };
+
+            var request = new EditUserRequest
+            {
+                PasswordRequest = passwordRequest,
+                UserInfo = infoRequest
+            };
+
+            Assert.ThrowsAsync<BadRequestException>(() => command.Execute(request));
         }
     }
 }
