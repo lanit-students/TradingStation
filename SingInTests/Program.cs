74c0d688e79a3c2761892662ced9ef5065a29c21
ifelseelif <yougrysha@gmail.com>
2020-04-15 21:55:24 +0300

    added tests

---
+++ SingInTests/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SingInTests
+{
+    internal class SignInTest
+    {
+        private static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://localhost:44335/signin";
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
