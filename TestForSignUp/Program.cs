b12c25abc5a3c97d9bb41bbc305362a88d0af957
Denis <den89181827071@mail.ru>
2020-04-16 18:48:05 +0300

    add handler

--- TestForSignUp/Program.cs
+++ TestForSignUp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium.Chrome;
+using System;
 using System.Threading;
 
 namespace TestForSignUp
@@ -7,17 +8,25 @@ namespace TestForSignUp
     {
         static void Main()
         {
-             var browser = new ChromeDriver();
-             browser.Navigate().GoToUrl("https://localhost:44335/signup");
-             Thread.Sleep(5000);
-             var input = new InputGenerator();
-             var filler = new ElementFiller(browser);
-             filler.Fill(input.commonString, input.emailString, input.dateString);
-             var buttons = browser.FindElementsByCssSelector("[type=\"button\"]");
-             buttons[1].Click();
-             Thread.Sleep(5000);
-             var submitButton = browser.FindElementByCssSelector("[type=\"Submit\"]");
-             submitButton.Click();           
+            try
+            {
+                var browser = new ChromeDriver();
+                browser.Navigate().GoToUrl("https://localhost:44335/signup");
+                Thread.Sleep(5000);
+                var input = new InputGenerator();
+                var filler = new ElementFiller(browser);
+                filler.Fill(input.commonString, input.emailString, input.dateString);
+                var buttons = browser.FindElementsByCssSelector("[type=\"button\"]");
+                buttons[1].Click();
+                Thread.Sleep(5000);
+                var submitButton = browser.FindElementByCssSelector("[type=\"Submit\"]");
+                submitButton.Click();
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Exception was detected");
+            }
+            Console.WriteLine("Successful");
         }
     }
 }
