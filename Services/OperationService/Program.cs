d648255c88882430feca69cea0e6470ac7968a19
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2020-04-18 16:18:47 +0300

    Some bugfixes

--- Services/OperationService/Program.cs
+++ Services/OperationService/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace BrokerService
+namespace OperationService
 {
     public class Program
     {
--- Services/OperationService/Startup.cs
+++ Services/OperationService/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace BrokerService
+namespace OperationService
 {
     public class Startup
     {
