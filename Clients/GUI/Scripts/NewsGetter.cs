655ae2d740c4732fcc9e1d7ec5428e79c9ef021b
Dmitry <st062458@student.spbu.ru>
2020-04-22 14:18:05 +0300

    Add news getter script

---
+++ Clients/GUI/Scripts/NewsGetter.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using DTO;
+using DTO.RestRequests;
+using Kernel;
+using Kernel.Enums;
+
+namespace GUI.Scripts
+{
+    public static class NewsGetter
+    {
+        public static async Task<NewsItem> GetNews(GetNewsRequest newsRequest)
+        {
+            const string url = "https://localhost:5007/getnews";
+
+            var client = new RestClient<GetNewsRequest, NewsItem>(url, RestRequestType.GET);
+
+            return await client.Execute();
+        }
+    }
+}
