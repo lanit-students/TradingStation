8e8e6acee94998367239211551b5b173abb00be0
Maksim Panasenko <maks.panasenko01@bk.ru>
2020-04-26 22:37:46 +0300

    Add logic for GetAllCurrenciesCommand

--- Services/BrokerService/Commands/GetAllCurrenciesCommand.cs
+++ Services/BrokerService/Commands/GetAllCurrenciesCommand.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using BrokerService.Interfaces;
+using BrokerService.Utils;
+using DTO;
+using DTO.MarketBrokerObjects;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace BrokerService.Commands
 {
-    public class GetAllCurrenciesCommand
+    public class GetAllCurrenciesCommand : IGetAllCurrenciesCommand
     {
+       public IEnumerable<Instrument> Execute(BrokerType broker, string token, int depth)
+        {
+            return BrokerFactory.Create(broker, token, depth).GetInstruments(InstrumentType.Currency);
+        }
     }
 }
