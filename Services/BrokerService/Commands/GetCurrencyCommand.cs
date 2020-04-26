baf132ad8594734bbd9c00451f2f47e1436b433f
Maksim Panasenko <maks.panasenko01@bk.ru>
2020-04-26 21:59:41 +0300

    Create GetCurrencyCommand

---
+++ Services/BrokerService/Commands/GetCurrencyCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrokerService.Commands
+{
+    public class GetCurrencyCommand
+    {
+    }
+}
