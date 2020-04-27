61d4765aea47868e626869243c7544a73871c727
Polina Lobanova <49339312+strannick-rodion@users.noreply.github.com>
2020-04-28 00:22:23 +0300

    may

---
+++ Services/UserService/Interfaces/ISecretTokenEngine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserService.Interfaces
+{
+    public interface ISecretTokenEngine
+    {
+        public Guid GetToken(string email);
+        public string GetEmail(Guid token);
+    }
+}
+ 
