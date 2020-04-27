01cd46c0672d8c5f7e30278a1767e5ef340aba49
Maksim Panasenko <maks.panasenko01@bk.ru>
2020-04-27 03:24:41 +0300

    Add logger to BrokerController

--- Services/BrokerService/Commands/GetCurrencyCommand.cs
+++ Services/BrokerService/Commands/GetCurrencyCommand.cs
@@ -2,10 +2,8 @@
 using BrokerService.Utils;
 using DTO;
 using DTO.MarketBrokerObjects;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace BrokerService.Commands
 {
--- Services/BrokerService/Controllers/BrokerController.cs
+++ Services/BrokerService/Controllers/BrokerController.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using BrokerService.Interfaces;
 using DTO;
 using DTO.MarketBrokerObjects;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,6 +12,13 @@ namespace BrokerService.Controllers
     [Route("[controller]")]
     public class BrokerController : Controller
     {
+        private readonly ILogger<BrokerController> logger;
+
+        public BrokerController([FromServices] ILogger<BrokerController> logger)
+        {
+            this.logger = logger;
+        }
+
         [Route("getcurrency")]
         [HttpPost]
         public Instrument GetCurrency(
@@ -23,6 +28,8 @@ namespace BrokerService.Controllers
             [FromBody] string token,
             [FromBody] string currency)
         {
+            logger.LogInformation("Success");
+
             return command.Execute(broker, token, depth, currency);
         }
         [Route("getallcurrencies")]
@@ -33,6 +40,8 @@ namespace BrokerService.Controllers
             [FromBody] BrokerType broker,
             [FromBody] string token)
         {
+            logger.LogInformation("Success");
+
             return command.Execute(broker, token, depth);       
         }
     }
