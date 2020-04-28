0a05d1674a3a9657bdb7c1a2fb67ca9558b2a03b
Denis <den89181827071@mail.ru>
2020-04-28 12:48:19 +0300

    add a template for bot logic

---
+++ Services/OperationService/BotLogic/Bot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OperationService.BotLogic
+{
+    public class Bot: IBot
+    {
+        public Guid ID;
+        public Guid UserID;
+
+        void Run() { }
+
+        void Disable() { }
+
+        void GetStatus() { }
+
+    }
+}
---
+++ Services/OperationService/BotLogic/IBot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OperationService.BotLogic
+{
+    interface IBot
+    {
+        void Run() { }
+
+        void Disable() { }
+
+        void GetStatus() { }
+    }
+}
