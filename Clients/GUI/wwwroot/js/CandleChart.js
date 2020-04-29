0982a92aed2d91f8c85fed7e150a539869ff0bde
ifelseelif <yougrysha@gmail.com>
2020-04-29 09:34:22 +0300

    graphic changed

--- Clients/GUI/wwwroot/js/CandleChart.js
+++ Clients/GUI/wwwroot/js/CandleChart.js
@@ -1,17 +1,49 @@
 ﻿var chart;
+var lastMin = -1, lastMax = -1;
+
 function drawChart(e, name) {
     var options = {
         chart: {
-            type: 'candlestick',
-            height: 350
+            type: "candlestick",
+            height: 350,
+            zoom: {
+                autoScaleYaxis: true
+            }
         },
         series: [],
         title: {
             text: name,
-            align: 'center'
+            align: "center"
+        },
+        tooltip: {
+            enabled: true
+        },
+        annotations: {
+            xaxis: [
+                {
+                    x: "Oct 06 14:00",
+                    borderColor: "#00E396",
+                    label: {
+                        borderColor: "#00E396",
+                        style: {
+                            fontSize: "12px",
+                            color: "#fff",
+                            background: "#00E396"
+                        },
+                        orientation: "horizontal",
+                        offsetY: 7,
+                        text: "Annotation Test"
+                    }
+                }
+            ]
         },
         xaxis: {
-            type: 'datetime'
+            type: "datetime",
+            labels: {
+                formatter: function(val) {
+                    return dayjs(val).format("MMM DD HH:mm");
+                }
+            }
         },
         yaxis: {
             tooltip: {
@@ -19,7 +51,7 @@ function drawChart(e, name) {
             }
         },
         noData: {
-            text: 'Loading...'
+            text: "Loading..."
         },
         legend: {
             show: false
@@ -33,13 +65,15 @@ function drawChart(e, name) {
 
 function updateChart(data) {
     chart.appendSeries({
-        data: [
-            {
-                x: data.time,
-                y: [data.open, data.high, data.low, data.close]
-            }
-        ]
-    }, false);
+            data: [
+                {
+                    x: new Date(data.time),
+                    y: [data.open, data.high, data.low, data.close]
+                }
+            ]
+        },
+        false);
+
 }
 
 function updateOptions(data) {
