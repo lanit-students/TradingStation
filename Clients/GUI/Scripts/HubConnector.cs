6cda4904a218f7a692b3acf228a6e960c6de97d8
ifelseelif <yougrysha@gmail.com>
2020-04-29 16:16:17 +0300

    fixed name

---
+++ Clients/GUI/Scripts/HubConnector.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+using ChartJs.Blazor.ChartJS.Common.Time;
+using DTO;
+using DTO.BrokerRequests;
+using DTO.MarketBrokerObjects;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace GUI.Scripts
+{
+    public static class HubConnector
+    {
+        private static Timer timer;
+        public static async Task SubscribeOnCandle(Action<Candle> onReceivedAction, Action onErrorAction, String token, String Figi)
+        {
+
+            var hubConnection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:5009/CandleHub")
+                .Build();
+
+            hubConnection.On<Candle>("ReceiveMessage", Candle =>
+            {
+                onReceivedAction.Invoke(Candle);
+                timer.Stop();
+                timer.Dispose();
+            });
+
+            await hubConnection.StartAsync();
+
+            await hubConnection.SendAsync("Subscribe", new SubscribeOnCandleRequest
+            {
+                Token = token,
+                Broker = BrokerType.TinkoffBroker,
+                Figi = Figi
+            });
+
+            timer = new Timer(5000);
+            timer.Elapsed += async (source, e) =>
+            {
+                onErrorAction.Invoke();
+                await hubConnection.StopAsync();
+                timer.Stop();
+                timer.Dispose();
+            };
+            timer.AutoReset = false;
+            timer.Start();
+        }
+    }
+}
