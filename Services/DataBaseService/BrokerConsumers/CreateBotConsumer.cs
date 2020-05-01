94c0f9bfe837d10681a3e9204a7397999b375917
Maksim Panasenko <maks.panasenko01@bk.ru>
2020-05-01 17:30:29 +0300

    Create logic for CreateBotConsumer

--- Services/DataBaseService/BrokerConsumers/CreateBotConsumer.cs
+++ Services/DataBaseService/BrokerConsumers/CreateBotConsumer.cs
@@ -1,11 +1,34 @@
-﻿using System;
+﻿using DataBaseService.Repositories.Interfaces;
+using DTO.RestRequests;
+using Kernel;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DataBaseService.BrokerConsumers
 {
-    public class CreateBotConsumer
+    public class CreateBotConsumer : IConsumer<CreateBotRequest>
     {
+        private readonly IUserRepository userRepository;
+
+        public CreateBotConsumer([FromServices] IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        private bool CreateBot(CreateBotRequest request)
+        {
+            return true;
+        }
+
+        public async Task Consume(ConsumeContext<CreateBotRequest> context)
+        {
+            var response = OperationResultWrapper.CreateResponse(CreateBot, context.Message);
+
+            await context.RespondAsync(response);
+        }
     }
 }
