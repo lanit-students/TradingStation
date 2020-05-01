859d7e456dfeb1fe42f5fd9b70e308ae5d559076
Maksim Panasenko <maks.panasenko01@bk.ru>
2020-05-01 22:08:48 +0300

    Add Configuration for DbBot

--- Services/DataBaseService/Database/Models/DbBot.cs
+++ Services/DataBaseService/Database/Models/DbBot.cs
@@ -1,11 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
 
 namespace DataBaseService.Database.Models
 {
     public class DbBot
     {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public bool IsActive { get; set; }
+    }
+
+    public class DbBotConfiguration : IEntityTypeConfiguration<DbBot>
+    {
+        public void Configure(EntityTypeBuilder<DbBot> builder)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
