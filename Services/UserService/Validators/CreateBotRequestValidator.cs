16bc5da6e79606ca6a9259166a71f5912f4145c8
Maksim Panasenko <maks.panasenko01@bk.ru>
2020-05-01 16:55:32 +0300

    Add CreateBotRequestValidator

---
+++ Services/UserService/Validators/CreateBotRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using DTO.RestRequests;
+using FluentValidation;
+
+namespace UserService.Validators
+{
+    public class CreateBotRequestValidator : AbstractValidator<CreateBotRequest>
+    {
+        public CreateBotRequestValidator()
+        {
+            RuleFor(request => request.userId)
+                .NotEmpty()
+                .WithMessage(ErrorsMessages.IdIsNullOrEmpty);
+        }
+    }
+}
