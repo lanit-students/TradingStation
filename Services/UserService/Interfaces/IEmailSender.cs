0a5f5ebdf2f50baeb4dab40af48e71f4d966644e
Polina Lobanova <49339312+strannick-rodion@users.noreply.github.com>
2020-05-02 15:05:52 +0300

    IEmailSender

---
+++ Services/UserService/Interfaces/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserService.Interfaces
+{
+   public interface IEmailSender
+    {
+        void SendEmail(string email, ISecretTokenEngine secretTokenEngine);
+    }
+}
