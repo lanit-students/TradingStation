d381ad04de93e16a328b8e47cec6791aef868158
Dmitry <st062458@student.spbu.ru>
2020-05-12 13:26:20 +0300

    Add consumer for BotRules

---
+++ Services/DataBaseService/BrokerConsumers/SaveBotRuleConsumer.cs
@@ -0,0 +1,32 @@
+﻿using DataBaseService.Repositories.Interfaces;
+using DTO;
+using Kernel;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DataBaseService.BrokerConsumers
+{
+    public class SaveRuleConsumerConsumer : IConsumer<BotRule>
+    {
+        private readonly IBotRuleRepository botRuleRepository;
+
+        public SaveRuleConsumerConsumer([FromServices] IBotRuleRepository botRuleRepository)
+        {
+            this.botRuleRepository = botRuleRepository;
+        }
+
+        private bool SaveRule(BotRule rule)
+        {
+            botRuleRepository.SaveRuleForBot(rule);
+            return true;
+        }
+
+        public async Task Consume(ConsumeContext<BotRule> context)
+        {
+            var response = OperationResultWrapper.CreateResponse(SaveRule, context.Message);
+
+            await context.RespondAsync(response);
+        }
+    }
+}
