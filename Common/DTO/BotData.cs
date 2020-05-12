33d28c9eb9ab0a8c4a8a0cb747659fdba13b7c11
Denis <den89181827071@mail.ru>
2020-05-12 16:39:00 +0300

    add get set for serialization

--- Common/DTO/BotData.cs
+++ Common/DTO/BotData.cs
@@ -5,10 +5,10 @@ namespace DTO
 {
     public class BotData
     {
-        public Guid Id;
-        public Guid UserId;
-        public string Name;
-        public List<BotRuleData> Rules;
-        public bool IsRunning;
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public List<BotRuleData> Rules { get; set; }
+        public bool IsRunning { get; set; }
     }
 }
