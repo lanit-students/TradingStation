6ff7f8d22a6917d591dc2b9da0c79598442c8d60
Dmitry <st062458@student.spbu.ru>
2020-05-12 12:49:16 +0300

    Add models to DTO

---
+++ Common/DTO/BotRule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    public class BotRule
+    {
+        public Guid Id { get; set; }
+
+        public int OperationType { get; set; }
+
+        public int TimeMarker { get; set; }
+
+        public int TriggerValue { get; set; }
+    }
+}
---
+++ Common/DTO/BrokerRequests/InternalSaveRuleRequest.cs
@@ -0,0 +1,13 @@
+﻿using DTO.MarketBrokerObjects;
+using System;
+
+namespace DTO.BrokerRequests
+{
+    public class InternalSaveRuleRequest
+    {
+        public Guid Id { get; set; }
+        public OperationType Type { get; set; }
+        public int TimeMarker { get; set; }
+        public int TriggerValue { get; set; }
+    }
+}
