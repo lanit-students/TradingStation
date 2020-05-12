403ed805452c02927afa15808c0aa44033bd0f04
Dmitry <st062458@student.spbu.ru>
2020-05-12 12:50:31 +0300

    Create BotRule mapper

---
+++ Services/DataBaseService/Mappers/BotRuleMapper.cs
@@ -0,0 +1,31 @@
+﻿using DataBaseService.Database.Models;
+using DataBaseService.Mappers.Interfaces;
+using DTO;
+
+namespace DataBaseService.Mappers
+{
+    public class BotRuleMapper : IBotRuleMapper
+    {
+        public DbBotRule MapToDbRule(BotRule rule)
+        {
+            return new DbBotRule
+            {
+                Id = rule.Id,
+                OperationType = rule.OperationType,
+                TimeMarker = rule.TimeMarker,
+                TriggerValue = rule.TriggerValue
+            };
+        }
+
+        public BotRule MapToRule(DbBotRule dbRule)
+        {
+            return new BotRule
+            {
+                Id = dbRule.Id,
+                OperationType = dbRule.OperationType,
+                TimeMarker = dbRule.TimeMarker,
+                TriggerValue = dbRule.TriggerValue
+            };
+        }
+    }
+}
---
+++ Services/DataBaseService/Mappers/Interfaces/IBotRuleMapper.cs
@@ -0,0 +1,12 @@
+﻿using DataBaseService.Database.Models;
+using DTO;
+
+namespace DataBaseService.Mappers.Interfaces
+{
+    public interface IBotRuleMapper
+    {
+        DbBotRule MapToDbRule(BotRule rule);
+
+        BotRule MapToRule(DbBotRule dbRule);
+    }
+}
