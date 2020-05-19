129f85ecc4b2cda191b6197e6a639abcb10f09fb
dTarasova <d.al.tarasova@gmail.com>
2020-05-19 22:22:03 +0400

    Bot create model completed

--- Clients/GUI/ViewModels/BotCreationModel.cs
+++ Clients/GUI/ViewModels/BotCreationModel.cs
@@ -1,4 +1,5 @@
 ﻿using DTO;
+using GUI.CustomValidationAttributes;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -7,6 +8,8 @@ namespace GUI.ViewModels
     public class BotCreationModel
     {
         [Required(ErrorMessage = "Name is required")]
+        [StringLength(32, ErrorMessage = "Name is too long.")]
+        [NameValidation("Name")]
         public string Name { get; set; }
         public List<BotRuleData> Rules { get; set; }
         public BotCreationModel() { }
--- Clients/GUI/ViewModels/BotRuleModel.cs
+++ Clients/GUI/ViewModels/BotRuleModel.cs
@@ -1,13 +1,15 @@
 ﻿using DTO.MarketBrokerObjects;
 using System;
 
-namespace DTO
+namespace GUI.ViewModels
 {
-    public class BotRuleData
+    public class BotRuleModel
     {
+        public bool IsChoosen { get; set; }
         public OperationType OperationType { get; set; }
         public decimal MoneyLimitPercents { get; set; }
         public TimeSpan TimeMarker { get; set; }
         public decimal TriggerValue { get; set; }
+        public string Description { get; set; }
     }
 }
